25fb6f360c90a3a97cc2dc60859121a860a9963d
KimMinWoooo <als33396dn@gmail.com>
2024-10-21 09:15:21 +0900

    fix : 클러스터 구성을 변경합니다.

--- Terraform/cluster/eks.tf
+++ Terraform/cluster/eks.tf
@@ -1,12 +1,11 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-
-  cluster_name                             = var.cluster_name
-  cluster_version                          = var.cluster_version
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-
+    
+  cluster_name                   = var.cluster_name
+  cluster_version                = var.cluster_version
+  cluster_endpoint_public_access = true
+    
   cluster_addons = {
     vpc-cni = {
       before_compute = true
@@ -24,62 +23,34 @@ module "eks" {
       })
     }
   }
-
+    
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
+    
   create_cluster_security_group = false
   create_node_security_group    = false
-
+    
   eks_managed_node_groups = {
-    private = {
-      instance_types           = ["m5.large"]
-      force_update_version     = true
-      release_version          = var.ami_release_version
-      use_name_prefix          = false
-      iam_role_name            = "${var.cluster_name}-ng-private"
-      iam_role_use_name_prefix = false
-
-      min_size     = 3
-      max_size     = 3  # EC2 3개 고정
+    default = {
+      instance_types       = ["t3.medium"]
+      force_update_version = true
+      release_version      = var.ami_release_version
+    
+      min_size     = 2
+      max_size     = 3
       desired_size = 3
-
-      subnet_ids = module.vpc.private_subnets
-
+    
       update_config = {
         max_unavailable_percentage = 50
       }
-
-      labels = {
-        workshop-default = "yes"
-      }
-    }
-    publics = {
-      instance_types           = ["m5.large"]
-      force_update_version     = true
-      release_version          = var.ami_release_version
-      use_name_prefix          = false
-      iam_role_name            = "${var.cluster_name}-ng-public"
-      iam_role_use_name_prefix = false
-      map_public_ip_on_launch = true
-
-      min_size     = 3
-      max_size     = 3  # EC2 3개 고정
-      desired_size = 3
-
-      subnet_ids = module.vpc.public_subnets
-
-      update_config = {
-        max_unavailable_percentage = 50
-      }
-
+    
       labels = {
         workshop-default = "yes"
       }
     }
   }
-
+    
   tags = merge(local.tags, {
     "karpenter.sh/discovery" = var.cluster_name
   })
-}
+}    
